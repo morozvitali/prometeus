dcc93baabd6576a84f0e29cb5857761f59b28eec
morozvitali <morozvitali99@gmail.com>
2024-04-16 16:55:08 +0300

    git refresh

--- src/Lections/unit10_Reg/unit10_3/Main1.java
+++ src/Lections/unit10_Reg/unit10_3/Main1.java
@@ -1,18 +1,27 @@
 package Lections.unit10_Reg.unit10_3;
 
-public class Main {
-    /*
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+public class Main1 {
+    /*
     Деякі методи класу Matcher:
     ::::::::::::::::::::::::::::::::::
     find () - пошук збігів
     start () - end () для отримання позицій початку та кінця збігу
     replaceAll () використовується для заміни всіх збігів
     replaceFirst () для заміни дише першого збігу
-
      */
 
     public static void main(String[] args) {
         String text = "The cat is black. The hat is red";
+        String pattern = "cat";
+
+        Pattern compiledPattern = Pattern.compile(pattern);
+        Matcher matcher = compiledPattern.matcher(text);
+
+        while (matcher.find()) {
+            System.out.println("Match found at position " + matcher.start() + " " + (matcher.end() - 1 ));
+        }
     }
 }
---
+++ src/Lections/unit10_Reg/unit10_3/Main3.java
@@ -0,0 +1,34 @@
+package Lections.unit10_Reg.unit10_3;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main3 {
+
+    public static void main(String[] args) {
+
+    /*
+
+    Методи класy Matcher:
+
+    find () використовується для знаходження збігів в рядку.
+    start () Ta end () використовуються для отримання позицій початку та кінця збігу.
+    replaceAll() використовується для заміни всіх збігів.
+    replaceFirst () використовується для заміни лише першого збігу
+
+    */
+//
+
+        String text = "Hello, Java! Nice to see you!!";
+        String pattern = "Java";
+
+        Pattern compiledPattern = Pattern.compile(pattern);
+        Matcher matcher = compiledPattern.matcher(text);
+
+        String removedText = matcher.replaceAll("Genya");
+        System.out.println("Removed text: " + removedText);
+
+    }
+}
